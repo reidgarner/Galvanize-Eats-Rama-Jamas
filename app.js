e16fabca82b9168661e8a787daed2377d2ac57b7
reidgarner <reidbgarner@gmail.com>
2018-10-09 16:59:47 -0600

    Finished JS and posted

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var grandTotal = 0
+    var itemList = []
     fetch('https://galvanize-eats-api.herokuapp.com/menu')
         .then((food) => food.json())
         .then(function (food) {
@@ -10,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 menuItems.classList.add('menu-items', 'waves-effect', 'waves-grey', 'btn-flat')
                 menuItems.setAttribute('id', 'selectedItem')
                 menuItems.innerHTML = food.menu[i].name + ' $' + food.menu[i].price
-                console.log(food.menu[i])
             }
             return food
         }).then(function (data) {
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
             for (let i = 0; i < selected.length; i++) {
                 selected[i].addEventListener('click', function selectedItem(event) {
                     event.target.setAttribute('value', 'food')
-                    console.log(event.target.innerText.split("$"))
                     let foodName = event.target.innerText.split("$")[0]
                     let foodPrice = event.target.innerText.split("$")[1]
                     let displayCont = document.querySelector('#displayCont')
@@ -28,11 +27,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     grandTotal += Number(foodPrice)
                     let spanTotal = document.querySelector('#displayCont span')
                     spanTotal.innerText = grandTotal.toFixed(2)
+                    itemList.push(foodName)
                 })
             }
             return data
         })
-
     let addItem = document.querySelector('.deliver')
     addItem.addEventListener('click', function () {
         let nameInput = document.querySelector('.info-input-1')
@@ -41,26 +40,28 @@ document.addEventListener("DOMContentLoaded", function (event) {
         let phoneNumber = phoneNumberInput.value
         let addressInput = document.querySelector('.info-input-3')
         let address = addressInput.value
-        let grandTotal = document
         event.preventDefault()
-        console.log(name)
-        console.log(phoneNumber)
-        console.log(address)
-
-    } )
-
-
-
-    // function selectItem(event) {
-    //     let displayCont = document.querySelector('#displayCont')
-    //     let quantity = document.querySelector('#')
-    // }
-    
-    // when you hit deliver
-    // it should grab the values from the form: name, phone, address
-    // it should grab the total and list of items
-        // create list of items global variable
-        // add items to it every time an item is added to the menu
-    // submit a post request with the form values and menu totals
-    // display post response
+        var finalList = {
+            name: name,
+            phoneNumber: phoneNumber,
+            address,
+            itemList,
+            grandTotal
+        }
+        var postURL = 'https://galvanize-eats-api.herokuapp.com/orders'
+    var settings = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(finalList)
+    }
+    fetch(postURL, settings)
+        .then(function(response) {
+            return response.text()
+        })
+        .then(function(body) {
+            document.body.innerHTML = `<h1>${body}</h1>`
+        })
+    })
 })
